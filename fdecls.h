9ad2eaff1e1ddb158719c1fde1c0796840d70a69
Michaeljohn Clement <inimino@gmail.com>
2025-12-28 23:18:24 +0000

    .gitignore

--- fdecls.h
+++ fdecls.h
@@ -168,6 +168,7 @@ void handle_agents();
 void handle_prompt(int block_idx);
 void handle_checksum(void);
 void handle_wants();
+void handle_wants_status();
 void handle_agents_wants();
 void grep_blocks(span pattern);
 void after(span arg);
